0ef96334dab26f733eb797fc29345572cc6f95e0
Manila Bui <manila.bui@gmail.com>
2020-05-01 22:15:17 -0500

    all results can be consoled from single file

--- day_1/script.js
+++ day_1/script.js
@@ -7,7 +7,7 @@ fs.readFile('day_1/input.txt', (err, data) => {
 	const floorsDown =  input.split('(').join('').length;
 	const result = floorsUp - floorsDown;
 
-	console.log(result);
+	console.log('1.1', result);
 })
 
 // part 1
@@ -15,7 +15,7 @@ fs.readFile('day_1/input.txt', (err, data) => {
 	const input = data.toString().split('');
     const result = input.reduce((acc, currChar) => currChar === '(' ? ++acc : --acc, 0);
 
-	console.log(result);
+	console.log('1.1', result);
 });
 
 // part 2
@@ -25,6 +25,6 @@ fs.readFile('day_1/input.txt', (err, data) => {
 
 	for (let i = 0; i < input.length; i++) {
 		input[i] === '(' ? currFloor++ : currFloor--;
-		if (currFloor < 0) return console.log(i + 1);
+		if (currFloor < 0) return console.log('1.2', i + 1);
 	}
 });
